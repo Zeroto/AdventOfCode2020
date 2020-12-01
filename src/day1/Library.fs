51727622cfb090032f5bd6b83765376eae645901
Sander Homan <sander.homan@wordtango.net>
2020-12-01 14:50:23 +0100

    Changed logic to use mutable variables Added optimization check to part 2. This results in a 100* speed increase

--- src/day1/Library.fs
+++ src/day1/Library.fs
@@ -12,31 +12,43 @@ let main (argv: string array) =
     System.IO.File.ReadAllText argv.[1]
     |> (fun s -> s.Split('\n'))
     |> Array.choose parseInt
-    |> Array.indexed
+  let count = Array.length input  
 
   match argv.[0] with
   | "a" ->
-    let result =
-      input
-      |> Array.pick
-          (fun (i, v) ->
-            input.[i+1..]
-            |> Array.tryFind (fun (_, s) -> v+s = 2020)
-            |> Option.map (fun (_, s) -> v*s)
-          )
+    let mutable i = 0
+    let mutable pending = true
+    let mutable result = 0
+    while pending do
+      let mutable i2 = i+1;
+      while pending && i2 < count do
+        if input.[i] + input.[i2] = 2020 then
+          pending <- false
+          result <- input.[i] * input.[i2]
+        else
+          i2 <- i2 + 1
+      i <- i+1
     string result
   | "b" ->
-    let result =
-      input
-      |> Array.pick
-          (fun (i, a) ->
-            input.[i+1..]
-            |> Array.tryPick
-              (fun (i2, b) ->
-                input.[i2+1..]
-                |> Array.tryFind (fun (_, c) -> a+b+c = 2020)
-                |> Option.map (fun (_, c) -> a*b*c)
-              )
-          )
+    let sw = Diagnostics.Stopwatch.StartNew();
+    let mutable i = 0
+    let mutable pending = true
+    let mutable result = 0
+    while pending do
+      let mutable i2 = i+1;
+      while pending && i2 < count do
+        let t = input.[i] + input.[i2]
+        if t < 2020 then
+          let mutable i3 = i2+1;
+          while pending && i3 < count do
+            if t + input.[i3] = 2020 then
+              pending <- false
+              result <- input.[i] * input.[i2] * input.[i3]
+            else
+              i3 <- i3 + 1
+        i2 <- i2 + 1  
+      i <- i+1
+    sw.Stop()
+    printfn "elapsed: %O" sw.Elapsed
     string result
   | _ -> null
