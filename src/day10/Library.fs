8752c5cef8de44d3c7671a5d9649fda4b4beabde
Sander Homan <sander.homan@wordtango.net>
2020-12-10 20:06:31 +0100

    Removed unused code in day 10

--- src/day10/Library.fs
+++ src/day10/Library.fs
@@ -4,13 +4,6 @@ open System
 
 let parseInput = Array.map int
 
-let goldenRatio = 1.618034
-let sqrt5 = Math.Sqrt(5.0)
-let Fibonacci n =
-  (Math.Pow(goldenRatio, float n) - Math.Pow((1.0 - goldenRatio), float n)) / sqrt5
-  |> Math.Round
-  |> int64
-
 let sqrt33 = Math.Sqrt 33.0
 let denom = (Math.Pow(586.0 + 102.0*sqrt33, 2.0/3.0) + 4.0 - 2.0 * Math.Pow(586.0 + 102.0 * sqrt33, 1.0/3.0))
 let alpha = 1.0/3.0*Math.Pow(19.0+3.0*sqrt33, 1.0/3.0) + 1.0/3.0*Math.Pow(19.0-3.0*sqrt33, 1.0/3.0) + 1.0/3.0
@@ -23,10 +16,6 @@ let main (argv: string array) =
   let input =
     File.ReadAllLines argv.[1]
     |> parseInput
-
-  [1..10]
-  |> List.map tribonacci
-  |> printfn "%A"
 
   match argv.[0] with
   | "a" ->
