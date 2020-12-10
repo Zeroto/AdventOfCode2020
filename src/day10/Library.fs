92ef78453b4a4a6b43145bad69fb525ac5bde109
Sander Homan <sander.homan@wordtango.net>
2020-12-10 20:05:36 +0100

    Fixed the formula for day10

--- src/day10/Library.fs
+++ src/day10/Library.fs
@@ -11,10 +11,22 @@ let Fibonacci n =
   |> Math.Round
   |> int64
 
+let sqrt33 = Math.Sqrt 33.0
+let denom = (Math.Pow(586.0 + 102.0*sqrt33, 2.0/3.0) + 4.0 - 2.0 * Math.Pow(586.0 + 102.0 * sqrt33, 1.0/3.0))
+let alpha = 1.0/3.0*Math.Pow(19.0+3.0*sqrt33, 1.0/3.0) + 1.0/3.0*Math.Pow(19.0-3.0*sqrt33, 1.0/3.0) + 1.0/3.0
+let beta = Math.Pow(586.0 + 102.0 * sqrt33, 1.0/3.0)
+let tribonacci n =
+  Math.Round ((Math.Pow(alpha, float n) * beta) / denom * 3.0)
+  |> int64
+
 let main (argv: string array) =
   let input =
     File.ReadAllLines argv.[1]
     |> parseInput
+
+  [1..10]
+  |> List.map tribonacci
+  |> printfn "%A"
 
   match argv.[0] with
   | "a" ->
@@ -29,7 +41,7 @@ let main (argv: string array) =
     let result = (counts |> Map.find 1) * ((counts |> Map.find 3) + 1)
     sprintf "Result: %d" result
   | "b" ->
-    let countToPermCount i = Math.Max((Fibonacci (i+1)) - 1L, 1L)
+    let countToPermCount i = tribonacci i
     let max = input |> Array.max
     let input = input |> Array.append [|0; max + 3|] |> Array.sort |> Array.pairwise
     let result =
