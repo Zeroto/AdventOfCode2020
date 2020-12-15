8f4d18614d7fbb858a921d5e4f5f631added1b04
Sander Homan <sander.homan@wordtango.net>
2020-12-15 13:47:10 +0100

    Switched to a mutable dictionary instead of map in day15 part 2
    This reduces the time from 80 seconds to 3 seconds

--- src/day15/Library.fs
+++ src/day15/Library.fs
@@ -1,6 +1,7 @@
 ﻿module Day15
 open System.IO
 open System
+open System.Collections.Generic
 
 let parseInput = Array.map int64 >> Array.mapi (fun i k -> (k,int64 i))
 
@@ -24,16 +25,20 @@ let main (argv: string array) =
     let result = iterate (int64 (input |> Array.length)-1L) (input |> Array.last |> fst) seed
     sprintf "Result: %d" result
   | "b" ->
-    let seed = input |> Array.rev |> Array.tail |> Array.rev |> Map.ofArray
-    let rec iterate i lastNumber numbers =
-      if i % 100000L = 0L then printfn "%d" i
+    let seed = input |> Array.rev |> Array.tail |> Array.rev |> dict |> Dictionary
+    let rec iterate i lastNumber (numbers: Dictionary<int64,int64>) =
+      if i % 500000L = 0L then printfn "%d" i
       if i = 29999999L then
         lastNumber
       else
-        let lastTimeSpoken = numbers |> Map.tryFind lastNumber |> Option.defaultValue i
-        let difference = i - lastTimeSpoken
-        let newMap = numbers |> Map.add lastNumber i
-        iterate (i+1L) difference newMap
+        let success, lastTimeSpoken = numbers.TryGetValue(lastNumber)
+        let difference =
+          if success then
+            i - lastTimeSpoken
+          else
+            0L
+        numbers.[lastNumber] <- i
+        iterate (i+1L) difference numbers
 
     let result = iterate (int64 (input |> Array.length)-1L) (input |> Array.last |> fst) seed
     sprintf "Result: %d" result
